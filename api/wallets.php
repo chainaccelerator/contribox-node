a79eefe13f125ad450f3ed48ec7769676c8ed9cd
ncantu <nicolas.cantu@instriit.com>
2021-05-09 22:25:16 +0200

    init gitignore

--- api/wallets.php
+++ api/wallets.php
@@ -100,6 +100,11 @@ span, p {
     margin-top: 0px;
     margin-bottom: 0px;
 }
+aside {
+    display: block;
+    float: left;
+    width: 200px;
+}
 .wallet {}
 .address {
     font-size: 10px;
@@ -149,10 +154,53 @@ span, p {
     </style>
 </head>
 <body>
-<?php echo $b_block; ?>
-<?php echo $b_peg; ?>
-<?php echo $block; ?>
-<?php echo $peg; ?>
+<section>
+    <p>Sats - LSats</p>
+</section>
+<section>
+    <form>
+        <fieldset><legend>To sign</legend>
+            <input type="integer" name="xxxxxx[amount]" readonly> LSat following <select name="xxxxxx[template]"> readonly</select> to <input type="text" name="xxxxxx[address]" value="address to" readonly> sign with <input type="text" name="xxxxxx[value]"> <input type="submit" name="xxxxxx[sign]" value="Sign"><br>
+            <input type="integer" name="yyyyyy[amount]" readonly> LSat following <select name="yyyyyy[template]" readonly></select> to <input type="text" name="yyyyyy[address]" value="address to" readonly> sign with <input type="text" name="yyyyyy[value]"> <input type="submit" name="yyyyyy[sign]" value="Sign"><br>
+        </fieldset>
+        <fieldset><legend>To complete</legend>
+            <input type="integer" name="xxxxxx[amount]" readonly> LSat following <select name="xxxxxx[template]"> readonly</select><br>
+            <input type="text" name="xxxxxx[address]" value="address to" readonly> sign with <input type="text" name="xxxxxx[value]" readonly><br>
+            <input type="text" name="xxxxxx[address]" value="address to" readonly> sign with <input type="text" name="xxxxxx[value]" readonly><br>
+            to sign <input type="text" name="xxxxxx[address]" value="address to" readonly><br>
+            to sign <input type="text" name="xxxxxx[address]" value="address to" readonly>
+            <br>
+            <input type="integer" name="xxxxxx[amount]" readonly> LSat following <select name="xxxxxx[template]"> readonly</select><br>
+            <input type="text" name="xxxxxx[address]" value="address to" readonly> signed with <input type="text" name="xxxxxx[value]" readonly><br>
+            <input type="text" name="xxxxxx[address]" value="address to" readonly> signed with <input type="text" name="xxxxxx[value]" readonly><br>
+            to sign <input type="text" name="xxxxxx[address]" value="address to" readonly><br>
+            to sign <input type="text" name="xxxxxx[address]" value="address to" readonly>
+        </fieldset>
+        <fieldset><legend>Send</legend>
+            <input type="integer" name="to[amount]"> LSat with <select name="to[utxo]" multiple></select> following <select name="to[template]"></select> to <input type="text" name="to[address]" value="address to"><br>
+            <input type="checkbox" name="mine[state]"> propose (min 5 000 sats) <input type="integer" name="mine[amount]" min="5000" value="5000"> LSats with <select name="mine[utxo]" multiple></select> following <select name="mine[template]"></select> to <select name="mine[address]" multiple></select> for mine (each)<br>
+            <input type="checkbox" name="pegIn[state]"> propose (min 5 000 sats) <input type="integer" name="pegIn[amount]" min="5000" value="5000"> Sats with <select name="pegIn[utxo]" multiple></select> following <select name="pegIn[template]"></select> to <select name="pegIn[address]" multiple></select> for peg in (each)<br>
+            <input type="checkbox" name="pegOut[state]"> propose (min 10 000 sats) <input type="integer" name="pegOut[amount]" min="10000" value="10000"> Sats with <select name="pegOut[utxo]" multiple></select> following <select name="pegOut[template]"></select> to <select name="pegOut[address]" multiple></select> for for peg out (each)<br>
+            <input type="checkbox" name="share[state]"> propose (min 10 000 sats) <input type="integer" name="share[amount]" min="10000" value="10000"> LSats with <select name="share[utxo]" multiple></select> following <select name="share[template]"></select> to <select name="share[address]" multiple></select> for share <input type="file" name="file">  (each)<br>
+            <input type="checkbox" name="backup[state]"> propose (min 1 000 sats) <input type="integer" name="backup[amount]" min="1000" value="1000"> LSats with <select name="backup[utxo]" multiple></select> following <select name="backup[template]"></select> to <select name="backup[address]" multiple></select> for backup (each)<br>
+            <input type="checkbox" name="lock[state]"> propose (min 10 000 sats) <input type="integer" name="lock[amount]" min="10000" value="1000"> LSats with <select name="lock[utxo]" multiple></select> following <select name="lock[template]"></select> to <select name="lock[address]" multiple></select> for lock (each)<br>
+            <input type="checkbox" name="witness[state]"> propose (min 10 sats) <input type="integer" name="witness[amount]" min="10" value="1000"> LSats with <select name="witness[utxo]" multiple></select> following <select name="witness[template]"></select> to <select name="witness[address]" multiple></select> for witness (each)<br>
+            <input type="checkbox" name="node[state]"> propose (min 10 sats) <input type="integer" name="node[amount]" min="10" value="1000"> LSats with <select name="node[utxo]" multiple></select> following <select name="node[template]"></select> to <select name="node[address]" multiple></select> for processing (each)<br>
+            <input type="submit" name="transaction" value="Send"><br>
+        </fieldset>
+        <fieldset><legend>Convert</legend>
+            <input type="integer" name="convertSat[amount]"> Sats with <select name="convertSat[utxo]" multiple></select> following <select name="convertSat[template]"></select> to <select name="convertSat[address]" multiple></select> for convert into LSAT<br>
+            <input type="integer" name="convertLSat[amount]"> LSats with <select name="convertLSAT[utxo]" multiple></select> following <select name="convertLSat[template]"></select> to <select name="convertLSAT[address]" multiple></select> for convert into SAT<br>
+            <input type="submit" name="conversion" value="Send"><br>
+        </fieldset>
+    </form>
+</section>
+<section>
+    <?php echo $b_block; ?>
+    <?php echo $b_peg; ?>
+    <?php echo $block; ?>
+    <?php echo $peg; ?>
+</section>
 </body>
 </html>
 
