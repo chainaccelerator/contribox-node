25ea82ad703637e43c9802b8ca6390a09e873d74
Nicolas Cantu <nicolas.cantu@instriit.com>
2021-05-28 13:06:35 +0200

    Update install_node.sh

--- install/install_node.sh
+++ install/install_node.sh
@@ -10,5 +10,10 @@ export FIRST_CONNECT=$3
 export EXTERNAL_IP=$4
 export NEW_NODE=1
 
+if [ -z $EXTERNAL_IP ];then
+  export IP=$(ip -j address | jq '.[1].addr_info[0].local')
+  export EXTERNAL_IP=$(eval echo $IP)
+fi
+
 source /var/www/contribox-node/install/install_sidechain.sh $BC_ENV $BC_USER "$EXTERNAL_IP" $FIRST_CONNECT
 
